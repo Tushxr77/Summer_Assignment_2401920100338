1fb05c583bfc05f4367f55890cbb4efd7d8b4809
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-06-15 22:29:13 +0530

    Day 1 Q3: Added LeetCode 876 - Middle of the Linked List solution

--- Week_3/Day_1/876_Middle_of_the_Linked_List.java
+++ Week_3/Day_1/876_Middle_of_the_Linked_List.java
@@ -3,7 +3,7 @@ class Solution {
     public ListNode middleNode(ListNode head) {
 
         ListNode slow = head;
-        ListNode fast = head;
+        ListNode fast = head; 
 
         while (fast != null && fast.next != null) {
 
