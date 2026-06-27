959d109d5cfaae310d3903b35f0403d680650d44
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-06-27 19:15:13 +0530

    Week 4 OOPS Q2: Added main class for nested class demonstration

---
+++ Week_4/OOPS/Question_2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outer outer = new Outer();
+
+        outer.display();
+
+        Outer.Inner inner = outer.new Inner();
+
+        inner.display();
+    }
+}
